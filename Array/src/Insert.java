1ad58a824b71fdd3f6f39473d6cf56c647048ea8
MeteorSis <uncleone@naver.com>
2017-03-08 17:46:01 +0900

    create Insert Practice

---
+++ Array/src/Insert.java
@@ -0,0 +1,23 @@
+class Insert {
+	
+	static void print(int[] a) {
+        for (int i : a)
+            System.out.printf("%d ", i);
+    }
+
+    static void insert(int[] a, int index, int value) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        System.out.println(a.length);
+        for (int i = 0; i < a.length; ++i)
+            a[i] = i;
+
+        insert(a, 5, -99);
+
+        print(a);
+
+    }
+}
